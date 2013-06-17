1ea9f7e7ae17b69a27d8f670054d21f995b532d3
carlosvega <carlosvega@gmx.es>
2013-06-18 00:24:45 +0200

    Changed the way httpDissector processes a list of files.
    The father acted as a seeder of children. One child per file but one after another.
    The child dies after processing the file and the father sows another child for the next file.
    Now this system have changed. No more children.
    Just a lonely father that processes all the files by himself one after another.
    
    In the previous version the hash table was destroyed after processing a file.
    
    So if a file has a request which is satisfied in another file, the dissector will never know and will discard that strange response.
    But now the table is not destroyed so the above problem disappears.
    
    Now the progress bar represents the progress for the list of files instead of file per file as before.
    
    The final output message has been simplified.
    
    Changes in NDleeTrazas.c and .h

--- NDleeTrazas.c
+++ NDleeTrazas.c
@@ -115,6 +115,7 @@ static int NDLTopen_next(NDLTdata_t *data, char *errbuf) {
 				if (errbuf) sprintf(errbuf, "Error: no se pudo leer la siguiente línea del fichero");
 				return 0;
 			}
+
 		} else return 0;
 		if (data->contFiles) NDLTclose_last(data);
 	} else {
@@ -139,6 +140,7 @@ static int NDLTopen_next(NDLTdata_t *data, char *errbuf) {
 			return 0;
 		}
 	}
+
 	data->contFiles++;
 	return 1;
 }
@@ -301,6 +303,7 @@ static int NDLTopen_next_multiple(NDLTdata_t *data, char *errbuf) {
 			return 0;
 		}
 	}
+
 	data->contFiles++;  // Que hacer cuando hay mas de un interfaz??
 	return 1;
 }
@@ -364,6 +367,7 @@ NDLTdata_t *NDLTabrirTraza(char *path, char *format, char *filter, int multi, ch
 	nuevo->interfaces=NULL;
 	
 	struct stat buf;
+	nuevo->nFiles = 0;//AÑADIDO
 	//Si el fichero es un file of files se van a guardar los paths de los ficheros en un arrays de estructuras. Una estructura por interfaz que se vea (interfaces separadas por una línea en blanco)
 	if(nuevo->multiFile){
 		char aux[TAM_LINE];
@@ -386,7 +390,8 @@ NDLTdata_t *NDLTabrirTraza(char *path, char *format, char *filter, int multi, ch
 	 				nuevo->bytesTotalesFicheros+=(unsigned long long)buf.st_size;
 	 				nuevo->interfaces[nuevo->numInterfaces-1].posALeerDeFichero=0;
 	 			}else{
-	 		 		if(strlen(aux)==1) lineaVacia=1; //si es una línea en blanco activo el flag lineaVacia 
+	 				nuevo->nFiles++; //AÑADIDO
+ 	 		 		if(strlen(aux)==1) lineaVacia=1; //si es una línea en blanco activo el flag lineaVacia 
 	 			 	else{  //sino es una línea en blanco y en la línea anterior no hay blancos significa que el interfaz tiene más ficheros. Se añade el fichero a la estructura ya creada para ese interfaz
 	 					lineaVacia=0;
 	 					if(nuevo->interfaces[nuevo->numInterfaces-1].numFicheros>=NUM_FICH_INTERFACE){
@@ -402,6 +407,7 @@ NDLTdata_t *NDLTabrirTraza(char *path, char *format, char *filter, int multi, ch
 	 		}
 	 	}	
 	}else{
+		nuevo->nFiles = 1; //AÑADIDO
 		stat(nuevo->path, &buf);
 		nuevo->bytesTotalesFicheros=(unsigned long long)buf.st_size;
 	}
