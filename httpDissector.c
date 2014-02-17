61c3381abda6282c71cba60da8716523324b08ec
Carlos Vega Moreno <Carlos Vega>
2014-02-17 23:39:29 +0100

    little fixes

--- httpDissector.c
+++ httpDissector.c
@@ -75,6 +75,7 @@ int main_process(char *format, char *filename);
 unsigned long remove_old_active_nodes(struct timespec last_packet);
 
 void sigintHandler(int signal){
+	signal(SIGINT, SIG_DFL);
 	struct timeval end;
 	gettimeofday(&end, NULL);
 
