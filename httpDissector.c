4335d8202a858215f4ee733a7d55b6459dc18cc0
Carlos Vega Moreno <Carlos Vega>
2014-03-25 16:41:37 +0100

    new version

--- httpDissector.c
+++ httpDissector.c
@@ -19,7 +19,7 @@ packet_info *pktinfo = NULL;
 
 #define GC_SLEEP_SECS 25
 
-char version[32] = "Version 2.71a";
+char version[32] = "Version 2.74";
 struct args_parse options;
 
 struct timespec last_packet;
