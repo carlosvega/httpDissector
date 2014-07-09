e40b9098c406af0343d80f14e7fddafbbeafad03
Carlos Vega Moreno <Carlos Vega>
2014-07-09 10:04:42 +0200

    dynamic hash

--- httpDissector.c
+++ httpDissector.c
@@ -19,7 +19,7 @@ packet_info *pktinfo = NULL;
 
 #define GC_SLEEP_SECS 25
 
-char version[32] = "Version 2.81";
+char version[32] = "Version 2.81_d";
 struct args_parse options;
 
 struct timespec last_packet;
