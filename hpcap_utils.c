2461682ad82f3e8a3f6829b7173489a7da8d026a
root <root@einsrepo.ii.uam.es>
2015-09-03 13:31:04 +0200

    ADDED CONDITIONAL COMPILING. make httpDissectorHPCAP for HPCAP4 support

--- hpcap_utils.c
+++ hpcap_utils.c
@@ -1,8 +1,12 @@
-#ifdef HPCAP_SUPPORT
 #include "hpcap_utils.h"
 #include "libmgmon.h"
 
 int hpcap_packet_online_loop(int cpu, int ifindex, int qindex, hpcap_handler callback, void *arg){
+	#ifdef HPCAP_SUPPORT
 	return mgmon_packet_online_loop(cpu, ifindex, qindex, callback, arg);
+	#endif
+	#ifndef HPCAP_SUPPORT
+	return -1;
+	#endif
 }
-#endif
+
