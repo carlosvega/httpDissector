637de43394baf8b6275202582135be34e27e86ae
Carlos Vega <carlos.vega@uam.es>
2015-09-03 13:13:06 +0200

    conditional compiling for HPCAP4 support

--- hpcap_utils.c
+++ hpcap_utils.c
@@ -1,6 +1,8 @@
+#ifdef HPCAP_SUPPORT
 #include "hpcap_utils.h"
 #include "libmgmon.h"
 
 int hpcap_packet_online_loop(int cpu, int ifindex, int qindex, hpcap_handler callback, void *arg){
 	return mgmon_packet_online_loop(cpu, ifindex, qindex, callback, arg);
 }
+#endif
