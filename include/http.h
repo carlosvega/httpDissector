464ff408314eec88340a52d509f27085a597b37d
Carlos Vega <carlosvega@CV-MBP.local>
2016-03-11 17:55:46 +0100

    added new prototypes, moved http op to structs

--- include/http.h
+++ include/http.h
@@ -12,16 +12,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <regex.h>
+// #include <pcre.h> 
+#include "tools.h"
+#include "args_parse.h"
+#include "dissector_structs.h"
 
 
 #define FREE(x) do { if ((x) != NULL) {free(x); x=NULL;} } while(0)
 #define MIN(a,b) (((a)<(b))?(a):(b))
-typedef enum {HEAD = 0, GET, POST, PUT, DELETE, TRACE, OPTIONS, CONNECT, PATCH, RESPONSE, ERR} http_op;
+
 
 typedef struct _internal_http_packet * http_packet;
 
 http_op http_which_method(u_char * tcp_payload);
 int http_parse_packet(u_char *tcp_payload, int length, http_packet *http_t, struct in_addr ip_src, struct in_addr ip_dst);
+
+http_op check_op_from_payload(u_char *tcp_payload, int length);
+int http_fill_event(u_char *tcp_payload, int length, http_event **event, http_op op);
+void print_http_event(http_event **event, FILE *output_file);
 
 int http_is_request(http_op h);
 char *http_op_to_char(http_op h);
