787bb7de268bc5a7bea943283bfc44d6a2e4d12a
carlosvega <carlosvm91@gmail.com>
2016-07-20 14:33:05 +0200

    removed useless stuff

--- include/index.h
+++
@@ -1,7 +0,0 @@
-#ifndef _index
-#define _index
-#include <stdio.h>
-#include "tools.h"
-void write_to_index_with_ts(long int position, struct timespec ts);
-void write_to_index(long int position);
-#endif
